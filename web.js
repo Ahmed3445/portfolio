8e145ac4ff46b33c1588be4254a4315c0c682253
Ahmed3445 <109922290+Ahmed3445@users.noreply.github.com>
2023-10-17 17:12:38 +0300

    Add files via upload

---
+++ web.js
@@ -0,0 +1,37 @@
+// scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+// Nav hamburgerburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+   // Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    // Check if the link has the no-scroll class
+    if (!link.classList.contains("no-scroll")) {
+      // If not, remove the show class from the ul element
+      ul.classList.remove("show");
+    }
+  })
+);
+ 
+  
